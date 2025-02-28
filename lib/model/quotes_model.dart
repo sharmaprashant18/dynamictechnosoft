f4640786faecc161fcd3de9d8ed1912cdc8bf98c
prashantsharmadynamic <prashantsharma.dtecnp@gmail.com>
2025-02-28 11:01:48 +0545

    Changed

--- lib/model/quotes_model.dart
+++ lib/model/quotes_model.dart
@@ -1,6 +1,20 @@
 class QuoteModel {
   List<Quotes> quotes;
   QuoteModel({required this.quotes});
+
+//Factory constructor
+  factory QuoteModel.formJson(Map<String, dynamic> json) {
+    return QuoteModel(
+      quotes: json['quotes'].map<Quotes>((e) => Quotes.fromJson(e)).toList(),
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'quotes': quotes.map((e) => e.toJson()).toList(),
+    };
+  }
+
+//copyWith method
   QuoteModel copyWith({List<Quotes>? quotes}) {
     return QuoteModel(quotes: quotes ?? this.quotes);
   }
@@ -11,6 +25,24 @@ class Quotes {
   String quote;
   String author;
   Quotes({required this.id, required this.quote, required this.author});
+
+//factory constructor
+  factory Quotes.fromJson(Map<String, dynamic> json) {
+    return Quotes(
+      id: json['id'],
+      quote: json['quote'],
+      author: json['author'],
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'quote': quote,
+      'author': author,
+    };
+  }
+
+//copyWith method
   Quotes copyWith({int? id, String? quote, String? author}) {
     return Quotes(
       id: id ?? this.id,
